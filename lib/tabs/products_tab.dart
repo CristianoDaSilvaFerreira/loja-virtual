04c5391f7159119f2a5ab3a35b4964ea2e3a3f27
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-03-07 13:23:46 -0300

    versão 1.4.1
    Exibindo as categorias dos produtos, e criando uma divisão entre os itens das categorias

--- lib/tabs/products_tab.dart
+++ lib/tabs/products_tab.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tiles/category_tile.dart';
 
 class ProductsTab extends StatelessWidget {
   @override
@@ -12,8 +13,16 @@ class ProductsTab extends StatelessWidget {
             child: CircularProgressIndicator(),
           );
         } else {
+          
+          // Colocando um divisor (linha entre os itens)
+          var dividedTiles = ListTile.divideTiles(
+            tiles: snapshot.data.documents.map((doc) {
+              return CategoryTile(doc);
+            }).toList(),
+            color: Colors.grey[500],
+          ).toList();
           return ListView(
-            children: <Widget>[],
+            children: dividedTiles,
           );
         }
       },
