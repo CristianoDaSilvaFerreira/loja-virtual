e70aa86e32e0e1914eb20d2ab324a6907372896a
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-03-07 11:01:00 -0300

    versão 1.3.1
    Personalizando o DrowerTile do menu lateral

---
+++ lib/tiles/drawer_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class DrawerTile extends StatelessWidget {
+  // Construtores
+  final IconData icon;
+  final String text;
+
+  DrawerTile(this.icon, this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        child: Container(
+          height: 60,
+          child: Row(
+            children: <Widget>[
+              Icon(
+                icon,
+                size: 32,
+                color: Colors.black,
+              ),
+              SizedBox(height: 32),
+              Text(
+                text,
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
