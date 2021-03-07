cf16ac3bd7df642fffc73dd15fcc4aa6374c0742
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-03-07 13:06:48 -0300

    versão 1.4
    Criando a tela de categoria de produtos

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../tabs/products_tab.dart';
 import '../widgets/custom_drawer.dart';
 import '../tabs/home_tab.dart';
 
@@ -17,7 +18,14 @@ class HomeScreen extends StatelessWidget {
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
         ),
-        Container(color: Colors.red),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
         Container(color: Colors.blue),
         Container(color: Colors.green),
         Container(color: Colors.grey),
