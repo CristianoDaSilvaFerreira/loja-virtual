e9c98ab13a07e630ef5b91368473bdc4c41e7f19
Cristiano Ferreira <cristianodevsystem@gmail.com>
2021-03-13 17:52:39 -0300

    versão 1.6.3
    Exibindo os itens e uma tela separada, para pode ter melhor visualização do produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class ProductScreen extends StatefulWidget {
   final ProductData product;
@@ -16,6 +15,55 @@ class _ProductScreenState extends State<ProductScreen> {
   _ProductScreenState(this.product);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Color primaryColor = Theme.of(context).primaryColor;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 4,
+              dotSpacing: 15,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              animationDuration: Duration(milliseconds: 2000),
+              indicatorBgPadding: 7,
+              showIndicator: true,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
